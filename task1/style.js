9fd71e1ac0dfd44e2bcbbec444a46579f80e2b6d
RuchirBeohar <88554327+RuchirBeohar@users.noreply.github.com>
2021-09-08 13:54:52 +0530

    Add files via upload

---
+++ task1/style.js
@@ -0,0 +1,40 @@
+// Image SLider
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+function myFunction() {
+  // var x = document.getElementById("fhead");
+  // var text = "";
+  // var i;
+  // for (i = 0; i < x.length ;i++) {
+  //   text += x.elements[i].value + "<br>";
+  // }
+   console.log("we will resolve your issue soon!");
+}
